0fa5b63233d54c6909fa8021bb70b5ce7c4f77eb
Gavin Cabbage <gavin.cabbage@capitalone.com>
2016-05-03 15:38:52 -0400

    ex 4

--- src/client/app/contacts-app.ts
+++ src/client/app/contacts-app.ts
@@ -1,14 +1,14 @@
 import {Component} from 'angular2/core';
 import {ContactHeaderComponent} from './contact-header-component/contact-header-component';
 import {Contact} from './models/contact';
-import {CONTACT_DATA} from './data/contact-data'
+import {CONTACT_DATA} from './data/contact-data';
 
 @Component({
     selector: 'contacts-app',
     template: `
         <contact-header-component></contact-header-component>
         <ul class="collection">
-            <li class="collection-item avatar">
+            <li *ngFor="#contact of contacts" class="collection-item avatar">
                 <img [src]="contact.image" alt="" class="circle">
                 <span class="title">{{contact.name}}</span>
             </li>
@@ -17,20 +17,5 @@ import {CONTACT_DATA} from './data/contact-data'
     directives: [ContactHeaderComponent]
 })
 export class ContactsApp {
-
-    contact: Contact = {
-      id: 7,
-      name: 'Diana Ellis',
-      email: '',
-      phone: '',
-      birthday: '',
-      website: '',
-      image: '/assets/images/7.jpg',
-      address: {
-        street: '6554 park lane',
-        zip: '43378',
-        city: 'Rush',
-        country: 'United States'
-      }
-    };
+    contacts:Array<Contact> = CONTACT_DATA;
 }
